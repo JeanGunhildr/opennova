21cd00252a254531e6540e26b5c2cf2c83dc0563
JeanGunhildr <JeanGunhildr26@gmail.com>
2026-08-17 14:18:55 +0700

    init project

---
+++ app/(public)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="w-full h-screen flex justify-center items-center flex-col gap-4">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgTI2hzQjkjhHQCVdLwHdG4JP8r5aJ9LS64ju3Qq-1vvWGGkmBhI3ZMQP9&s=10" alt="" 
+            className="rounded-4xl w-60"/>
+            <p className="text-white font-semibold text-2xl ">Saya Akan Lawan!!</p>
+        </div>
+    )
+}
